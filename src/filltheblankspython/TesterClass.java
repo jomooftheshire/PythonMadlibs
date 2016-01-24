dddcde16dcb5a71a7d768e6bacab362d2f8e56d0
jomooftheshire <joshua_f_mulcock@hotmail.com>
2016-01-24 21:08:51 +0000

    Main Function Implemented
    The JLabels now switch places when pressing the X button. However
    automatic switching not working (potential issue with the code swapping
    or reference of the ArrayLists.

---
+++ src/filltheblankspython/TesterClass.java
@@ -0,0 +1,100 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package filltheblankspython;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author joshu
+ */
+public class TesterClass {
+    
+    public TesterClass(){
+        
+    }
+    
+    //void labelChecker(){
+//        boolean found = false;
+//        System.out.println("Code:");
+//        for (Word c : screenCode){        
+//            for (Word k : keyboard){
+//                if(c.getLabelName().equals(k.getLabelName())){
+//                    System.out.println("Found: " + "c:" + c.getWord() + ", k:" + k.getWord());
+//                    found = true;
+//                }
+//            }
+//        }
+//        if(found == false){
+//            System.out.println("No marches found");
+//        }
+//        }
+    
+//    private void objectchecker(){
+//        boolean found = false;
+//        System.out.println("Code:");
+//        for (Word c : screenCode){        
+//            for (Word k : keyboard){
+//                if(c.equals(k)){
+//                    System.out.println("Found: " + "c:" + c.getWord() + ", k:" + k.getWord());
+//                    found = true;
+//                }
+//            }
+//        }
+//        if(found == false){
+//            System.out.println("No marches found");
+//        }
+//    }
+    
+//    private void printLabelPoints(){
+//        System.out.println("Code:");
+//        for(Word c : screenCode){
+//            try {
+//                System.out.println(c.getWord() + ":" + "[" + c.getLabel().getX() + "," + c.getLabel().getY() + "]");
+//            }
+//            catch (java.awt.IllegalComponentStateException x){
+//                System.out.println("No Place:" + c.getWord());
+//            }
+//        }
+//        
+//        System.out.println();
+//        System.out.println("Keyboard");
+//        for(Word k : keyboard){
+//            try {
+//                System.out.println(k.getWord() + ":" + "[" + k.getLabel().getX() + "," + k.getLabel().getY() + "]");
+//            }
+//            catch (java.awt.IllegalComponentStateException x){
+//                System.out.println("No Place:" + k.getWord());
+//            }
+//        }
+//        
+//    }
+        
+      //public static void printLabelName(ArrayList<Word> screenCode){
+//        System.out.println("Code:");
+//        for(Word c : screenCode){
+//            try {
+//                System.out.println(c.getLabelName());
+//            }
+//            catch (java.awt.IllegalComponentStateException x){
+//                System.out.println("No Label:" + c.getWord());
+//            }
+//        }
+//        
+//        System.out.println();
+//        System.out.println("Keyboard");
+//        for(Word k : keyboard){
+//            try {
+//                System.out.println(k.getLabelName());
+//            }
+//            catch (java.awt.IllegalComponentStateException x){
+//                System.out.println("No Label:" + k.getWord());
+//            }
+//        }
+//        
+//    }
+    
+}
